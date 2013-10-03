0514533305931f07c7ce24ef7807f10cb662cf5c
Bryan Stanfill <stanfill@iastate.edu>
2013-10-03 14:14:05 -0500

    add winsorized mean

--- robustFunctions.R
+++ robustFunctions.R
@@ -20,7 +20,7 @@ ruarsCont<-function(n,rangle,kappa,p,S=id.SO3,Scont){
   
 }
 
-HnFun<-function(Qs){
+HnFun<-function(Qs,full=TRUE){
   #Compute the statistic proposed by FLW(?) that is a function of the largest eigenvalue
   #when observation i was removed
   
@@ -34,7 +34,12 @@ HnFun<-function(Qs){
     Tn1<-t(Qsi)%*%Qsi
     tau4n1[i]<-svd(Tn1)$d[1]
   }
-  Hn<-(n-2)*(1+tau4n1-tau4n)/(n-1-tau4n1)
+  
+  if(full){
+    Hn<- (n-2)*(1+tau4n1-tau4n)/(n-1-tau4n1)
+  }else{
+    Hn<- 1+tau4n1-tau4n
+  }
   return(Hn)
 }
 
@@ -61,7 +66,7 @@ trimMean<-function(Qs,a,discordFun,anneal=F){
   nCut<-floor(min(max(0,n*a),n)) #remove at least 0, at most n
   
   if(nCut==0){
-    return(mean(Qs))
+    return(list(Qs=Qs,Shat=mean(Qs)))
   }
   
   if(anneal){
@@ -70,11 +75,47 @@ trimMean<-function(Qs,a,discordFun,anneal=F){
       Hn<-discordFun(Qs)
       Qs<-as.Q4(Qs[-which.max(Hn),])
     }
-    return(mean(Qs))
+    return(list(Qs=Qs,Shat=mean(Qs)))
     
   }else{
     Hn<-discordFun(Qs)
     toCut<-which(order(Hn)>(n-nCut))
-    return(mean(as.Q4(Qs[-toCut,])))
+    tQs<-as.Q4(Qs[-toCut,])
+    return(list(Qs=tQs,Shat=mean(tQs)))
   }
+}
+
+winzMean<-function(Rs,a,discordFun,anneal=F){
+  #Project the most extreme a% observations closer
+  #Rs - the sample
+  #a - percent of sample to remove
+  #discordFun - function to identify extreme observations, larger value more extreme obs
+  #anneal - T/F, remove all at once (F) or one at a time (T)
+  n<-nrow(Rs)
+  nCut<-floor(min(max(0,n*a),n)) #project at least 0, at most n
+  
+  if(nCut==0){
+    return(mean(Qs))
+  }
+  Shat<-mean(Rs)
+  
+  if(anneal){
+    return(Shat)
+  }else{
+    
+    crs<-dist(Rs,Shat,method='intrinsic')
+    huberC<-sort(crs)[n-nCut] #There should be nCut observations further away than
+                              #this value
+    
+    toWinz<-which(crs>huberC)
+    badRs<-as.SO3(Rs[toWinz,]) #These are the observations to project closer to center
+    us<-axis(badRs)
+    winzRs<-SO3(us,rep(huberC,length(toWinz)))
+    winzRs<-center(winzRs,t(Shat))
+    wRs<-Rs
+    wRs[toWinz,]<-winzRs
+    
+    return(list(Rs=wRs,Shat=mean(Rs)))
+  }
+  
 }
