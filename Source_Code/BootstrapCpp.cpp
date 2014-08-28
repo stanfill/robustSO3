e0dbd77c78785a68cc128972d979291d84f97766
stanfill <bryan.stanfill@csiro.au>
2014-08-29 09:33:58 +1000

    remove unnecessary function now in rotations.h

--- Source_Code/BootstrapCpp.cpp
+++ Source_Code/BootstrapCpp.cpp
@@ -23,114 +23,7 @@ double kappaHatCpp(arma::mat Rbar){
   
 }
 
-//Necessary but uninteresting functions
-arma::rowvec RdistCArma(arma::mat Q1, arma::rowvec Q2){
-  /*Compute the geodesic distance between quaternions Q1 and Q2*/
-  /* Q1 must be an n-by-4 matrix with quaternion rows and Q2 a single (1x4) quaternion*/
-  
-  int n = Q1.n_rows, i=0; 
-	double cp;
-	arma::rowvec rs(n);
-	
-	for(i=0;i<n;i++){
-		
-		cp = sum(Q1.row(i)*Q2.t());
-		rs(i) = acos(2*cp*cp-1);
-		
-	}
-	
-	return rs;
-}
 
-arma::rowvec HnCpp(arma::mat Qs){
-  //Compute the Hn tests statistics
-  
-  int n = Qs.n_rows, i=0;
-  arma::mat T = Qs.t()*Qs;
-  arma::mat eigvec, eigvecJ;
-  arma::vec eigval, eigvalJ;
-  arma::eig_sym(eigval,eigvec,T);
-  arma::rowvec Hn(n);
-  arma::rowvec Qj;
-  arma::mat Tj;
-
-  for(i = 0;i<n; i++){
-    Qj = Qs.row(i);
-    
-    Tj = T-Qj.t()*Qj;
-    arma::eig_sym(eigvalJ,eigvecJ,Tj);
-    Hn(i)=(n-2)*(1+eigvalJ(3)-eigval(3))/(n-1-eigvalJ(3));
-    
-  }
-  return Hn;
-}
-
-// [[Rcpp::export]]
-arma::rowvec HnCppIntrinsic(arma::mat Qs){
-  
-  //Compute the intrinsic Hn tests statistics
-  
-  int n = Qs.n_rows, i=0, j=0;
-
-  //Get T matrix of whole sample to make it easier later on
-  arma::mat T = Qs.t()*Qs;
-  arma::rowvec Qhat = rotations::meanQ4C(Qs);
-  arma::rowvec dists(n);
-
-  //Sum of squared geometric distances between proj. mean and each obs
-  double SSE = 0.0, SSEJ=0.0;
-  dists = square(RdistCArma(Qs,Qhat));
-  SSE = sum(dists);
-
-  arma::rowvec Hn(n);
-  arma::rowvec Qhatj;
-  arma::mat QsJ(n,4);
-  
-  //Variables for reduced sample mean
-  arma::rowvec Qj, distsJ(n-1);
-  arma::mat Tj(4,4), eigvecJ(4,4);
-
-  for(i = 0;i<n; i++){
-    
-    QsJ.resize(n,4);
-    
-    for(j = 0; j<n; j++){
-      if(j!=i){
-        QsJ.row(j) = Qs.row(j);
-      }
-    }
-    
-    QsJ.shed_row(i);
-    
-    //Compute projected mean when jth row is cut out
-    Qhatj = rotations::meanQ4C(QsJ);
-    distsJ = square(RdistCArma(QsJ,Qhatj));
-    SSEJ = sum(distsJ);
-    
-    //Rcpp::Rcout << "SSEJ: " << SSEJ << std::endl;
-    
-    Hn(i)=(n-2)*(SSE-SSEJ)/(SSEJ);
-    
-  }
-
-  return Hn;
-  
-}
-
-NumericVector rcayleyCpp(int n, double kappa){
-  RNGScope scope;
-  NumericVector bet(n), alp(n), theta(n);
-  
-  bet = rbeta(n,kappa+0.5,1.5);
-  alp = rbinom(n,1,0.5);
-  
-  for(int i = 0; i<n; i++){
-    
-    theta[i] = acos(2*bet[i]-1)*(1-2*alp[i]);
-  
-  }
-  return theta;
-}
 
 // [[Rcpp::export]]
 arma::mat GenQ4Cpp(NumericVector rs){
@@ -190,16 +83,16 @@ arma::rowvec HnBootCpp(arma::mat Rs, int m, int type){
   
   if(type==1){
     for(i=0;i<m;i++){
-      rs = rcayleyCpp(n, kapHat);
+      rs = rotations::rcayleyCpp(n, kapHat);
       Qs = GenQ4Cpp(rs);
-      Hni = HnCppIntrinsic(Qs);
+      Hni = rotations::HnCppIntrinsic(Qs);
       Hn(i)= max(Hni);
     }
   }else{
     for(i=0;i<m;i++){
-      rs = rcayleyCpp(n, kapHat);
+      rs = rotations::rcayleyCpp(n, kapHat);
       Qs = GenQ4Cpp(rs);
-      Hni = HnCpp(Qs);
+      Hni = rotations::HnCpp(Qs);
       Hn(i)= max(Hni);
     } 
   }
